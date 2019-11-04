db02339732eba7d2053a915dc8062fa87f40bb11
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-11-04 01:09:19 -0300

    Update Ordenação por distribuição usando pilhas.c

--- Ordenação por distribuição usando pilhas.c
+++ Ordenação por distribuição usando pilhas.c
@@ -1,17 +1,14 @@
 /*
  * Nome: Antônio Pierre Martins Vieira
- * Data: 03/11/2019
+ * Data: 04/11/2019
  * JetBrains --> Clion IDE
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define MAXTAM 70
-#define TAM_VET_PILHA1 26
-#define TAM_VET_PILHA2 11
 
-typedef int TipoApontador;
+#define MAXTAM 70
 typedef char TipoChave[100];
+typedef int TipoApontador;
 
 typedef struct {
     TipoChave Nome;
@@ -19,159 +16,89 @@ typedef struct {
 } Aluno;
 
 typedef struct {
-    Aluno* alunos;
-    int qtde_alunos;
+    Aluno Item[MAXTAM];
     TipoApontador Topo;
-}PilhaAlunos;
+} TipoPilha;
 
-void FPVazia(PilhaAlunos *Pilha){
-    Pilha->Topo = 0;
-}
-
-void Empilha(Aluno x, PilhaAlunos *Pilha){
-    if (Pilha->Topo == MAXTAM){
-        printf(" Erro   pilha est  a  cheia\n");
-    }
-    else {
-        Pilha->Topo++;
-        Pilha->alunos[Pilha->Topo - 1].Nota = x.Nota;
-        strcpy(Pilha->alunos[Pilha->Topo - 1].Nome, x.Nome);
-    }
-}
-
-void imprimirPilha(PilhaAlunos pilhaAlunos){
-    for (int i = 0; i < pilhaAlunos.qtde_alunos; ++i) {
-        printf("%s\n%d\n", pilhaAlunos.alunos[i].Nome, pilhaAlunos.alunos[i].Nota);
-    }
-}
-
-int le_do_arquivo(Aluno *alunos2, const char *nome_arquivo){
+int le_do_arquivo(const char *nome_arquivo, Aluno *alunos){
     FILE *arq;
     int result;
+    // Abre um arquivo BINÁRIO para LEITURA
     arq = fopen(nome_arquivo, "rb");
     if (arq == NULL){// Se houve erro na abertura
         printf("Problemas na abertura do arquivo\n");
         exit(0);
     }
-    result = fread (alunos2, sizeof(Aluno), MAXTAM, arq);
+    result = fread (alunos, sizeof(Aluno), MAXTAM, arq);
     fclose(arq);
     printf("\n");
     return result;
 }
 
-void prepara_pilhas1(PilhaAlunos *pilhas, const int *v_qtde){
-    for (int i = 0; i < TAM_VET_PILHA1; ++i) {
-        FPVazia(&pilhas[i]);
-        pilhas[i].alunos = malloc(sizeof(Aluno)*v_qtde[i]);
-        pilhas[i].qtde_alunos = v_qtde[i];
-        pilhas[i].Topo = 0;
+void FPVazia(TipoPilha *Pilha){
+    Pilha->Topo = 0;
+}
+
+int Vazia(TipoPilha Pilha){
+    return (Pilha.Topo == 0);
+}
+
+void Empilha(Aluno x, TipoPilha *Pilha){
+    if (Pilha->Topo == MAXTAM){
+        printf(" Erro   pilha est  a  cheia\n");
+    }
+    else {
+        Pilha->Topo++;
+        Pilha->Item[Pilha->Topo - 1] = x;
     }
 }
 
-void prepara_pilhas2(PilhaAlunos *pilhas, const int *vet_notas){
-    for (int i = 0; i < TAM_VET_PILHA2; ++i) {
-        pilhas[i].alunos = malloc(sizeof(Aluno)*vet_notas[i]);
-        pilhas[i].qtde_alunos = vet_notas[i];
-        pilhas[i].Topo = 0;
+void Desempilha(TipoPilha *Pilha, Aluno *Item){
+    if (Vazia(*Pilha)){
+        printf(" Erro   pilha est  a  vazia\n");
+    }
+    else {
+        *Item = Pilha->Item[Pilha->Topo - 1];
+        Pilha->Topo--;
     }
 }
 
-int converte_ascII_em_posicao_vetorial(char caractere){
-    int posicao = -1;
-    if(caractere >= 65 && caractere <= 90){
-        posicao = caractere - 65;
-    }
-    else if(caractere >= 97 && caractere <= 122){
-        posicao = caractere - 97;
-    }
-    return posicao; //se retornar -1 é pq deu merda
-}
 
-void preenche_vet_qtde_alunos(const Aluno *aluno, int q, int *v){
-    for (int i = 0; i < q; ++i) {
-        v[converte_ascII_em_posicao_vetorial(aluno[i].Nome[0])]++;
+void Imprime(TipoPilha *Pilha){
+    TipoPilha Pilhaux;
+    Aluno x;
+    FPVazia(&Pilhaux);
+    while (!Vazia(*Pilha)){
+        Desempilha(Pilha, &x);
+        Empilha(x, &Pilhaux);
     }
-}
-
-void bucket_sort_alunos(Aluno *alunos, PilhaAlunos *pilhas, int qtde_alunos){
-    //Joga cada aluno em uma posição do vetor de pilhas, fazendo-se então um bucket sort
-    int pos_vet_pilha;
-    for (int i = 0; i < qtde_alunos; ++i) {
-        pos_vet_pilha = converte_ascII_em_posicao_vetorial(alunos[i].Nome[0]);
-        Empilha(alunos[i], &pilhas[pos_vet_pilha]);
+    while (!Vazia(Pilhaux)){
+        Desempilha(&Pilhaux, &x);
+        printf("%s\n%d\n", x.Nome, x.Nota);
     }
 }
 
-void ordenar_pilha(PilhaAlunos *pilha){//Inserção
-    Aluno aluno_aux;
-    for (int i = 0; i < pilha->qtde_alunos; ++i) {
-        for (int j = i+1; j < pilha->qtde_alunos; ++j) {
-            if(pilha->alunos[i].Nota > pilha->alunos[j].Nota){
-                aluno_aux = pilha->alunos[i];
-                pilha->alunos[i] = pilha->alunos[j];
-                pilha->alunos[j] = aluno_aux;
+void bucket_sort(Aluno *alunos, int qtde, TipoPilha *pilha){
+    for (int bucket_position = 0; bucket_position <= 10; ++bucket_position){//cada posição recorrente é a posição para um bucket
+        for (int i = 0; i < qtde; ++i) {
+            if(alunos[i].Nota == bucket_position){
+                Empilha(alunos[i], pilha);
             }
         }
     }
 }
 
-void ordenar_pilhas_separado(PilhaAlunos *pilhas){
-    for (int i = 0; i < TAM_VET_PILHA1; ++i) {
-        if(pilhas[i].qtde_alunos > 0){
-            ordenar_pilha(&pilhas[i]);
-        }
-    }
-}
-
-void imprimirPilhas(PilhaAlunos *pilhas, int q){
-    for (int i = 0; i < q; ++i) {
-        imprimirPilha(pilhas[i]);
-    }
-}
-
-void tira_de_uma_pilha_e_taca_na_outra(PilhaAlunos *pilhas1, PilhaAlunos *pilhas2){
-    int aux;
-    for (int pos_pilha1 = 0; pos_pilha1 < TAM_VET_PILHA1; ++pos_pilha1) {
-        aux = pilhas1[pos_pilha1].qtde_alunos;
-        while(aux > 0){
-            aux--;
-            Empilha(pilhas1[pos_pilha1].alunos[aux], &pilhas2[pilhas1[pos_pilha1].alunos[aux].Nota]);
-        }
-    }
-}
-
-void ordenar_alunos(Aluno *alunos, PilhaAlunos *pilhas, PilhaAlunos *pilhas2, int qtde_alunos){
-    bucket_sort_alunos(alunos, pilhas, qtde_alunos);//bukcket
-    ordenar_pilhas_separado(pilhas);//ordenação entre cada pilha
-    tira_de_uma_pilha_e_taca_na_outra(pilhas, pilhas2);
-}
-
-void preenche_vet_qtde_notas(Aluno *alunos, int q, int *v){
-    for (int i = 0; i < q; ++i) {
-        v[alunos[i].Nota]++;
-    }
-}
-
-int main(int args, char *arg[]){
-    int qtde_alunos, *vet_qtde_alunos, *vet_qtde_notas;
-    PilhaAlunos *pilhas, *pilhas2;
+int main(){
     Aluno *alunos;
+    int qtde_alunos;
     char nome_arquivo[100];
-    pilhas = malloc(sizeof(PilhaAlunos)*TAM_VET_PILHA1); //26 A - Z
-    pilhas2 = malloc(sizeof(PilhaAlunos)*TAM_VET_PILHA2);
+    TipoPilha pilha;
     alunos = malloc(sizeof(Aluno)*MAXTAM);
-    vet_qtde_alunos = calloc(TAM_VET_PILHA1, sizeof(int));
-    vet_qtde_notas = calloc(TAM_VET_PILHA2, sizeof(int));
+    FPVazia(&pilha);
     printf("Digite o nome do arquivo: ");
     scanf("%s", nome_arquivo);
-    qtde_alunos = le_do_arquivo(alunos, nome_arquivo);
-    preenche_vet_qtde_alunos(alunos, qtde_alunos, vet_qtde_alunos);
-    preenche_vet_qtde_notas(alunos, qtde_alunos, vet_qtde_notas);
-    prepara_pilhas1(pilhas, vet_qtde_alunos);
-    prepara_pilhas2(pilhas2, vet_qtde_notas);
-    ordenar_alunos(alunos, pilhas, pilhas2, qtde_alunos);
-    imprimirPilhas(pilhas2, TAM_VET_PILHA2);
-    free(pilhas);
-    free(pilhas2);
+    qtde_alunos = le_do_arquivo(nome_arquivo, alunos);
+    bucket_sort(alunos, qtde_alunos, &pilha);
+    Imprime(&pilha);
     return 0;
 }
